76020a874e389681459d1025987dfdd633d65c1f
yanjunlu <yanjunlu1980@gmail.com>
2018-04-19 00:11:57 +0800

    nio

---
+++ src/niotest/nio/TimeClientHandle.java
@@ -0,0 +1,114 @@
+package niotest.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class TimeClientHandle implements Runnable {
+    private String host;
+    private int port;
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private volatile boolean stop;
+    
+	public TimeClientHandle(String host, int port) {
+		this.host = host;
+		this.port = port;
+		
+		try {
+			selector = Selector.open();
+			socketChannel = SocketChannel.open();
+			socketChannel.configureBlocking(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+	}
+
+	public void run() {
+		try {
+			doConnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		while (!stop) {
+			try {
+				selector.select(1000);
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while (it.hasNext()) {
+					key = it.next();
+					it.remove();
+					handleInput(key);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
+
+	}
+
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			SocketChannel sc = (SocketChannel) key.channel();
+			if (key.isConnectable()) {
+				if (sc.finishConnect()) {
+					sc.register(selector, SelectionKey.OP_READ);
+					doWrite(sc);
+				} else {
+					System.exit(1);
+				}
+			}
+			if (key.isReadable()) {
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out.println("resp is :" + body);
+					this.stop = true;
+				} else if (readBytes < 0) {
+					key.cancel();
+					sc.close();
+				} else {
+					
+				}
+			}
+		}
+		
+	}
+
+	private void doConnect() throws IOException {
+		if (socketChannel.connect(new InetSocketAddress(host, port))) {
+			socketChannel.register(selector, SelectionKey.OP_READ);
+			doWrite(socketChannel);
+		} else {
+			socketChannel.register(selector, SelectionKey.OP_CONNECT);
+		}
+		
+	}
+
+	private void doWrite(SocketChannel sc) throws IOException {
+		byte[] req = "query time".getBytes();
+		ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+		writeBuffer.put(req);
+		writeBuffer.flip();
+		sc.write(writeBuffer);
+		if (!writeBuffer.hasRemaining()) {
+			System.out.println("send command to server success.");
+		}
+		
+	}
+
+}
--- src/niotest/nio/TimeServerHandler.java
+++ src/niotest/nio/TimeServerHandler.java
@@ -6,10 +6,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
 
 public class TimeServerHandler implements Runnable {
 	private Selector selector;
@@ -37,6 +41,24 @@ public class TimeServerHandler implements Runnable {
 		while (!stop) {
 			try {
 				selector.select(1000);
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while (it.hasNext()) {
+					key = it.next();
+					it.remove();
+					try {
+						handleInput(key);
+					} catch (Exception e) {
+						if (key != null) {
+							key.cancel();
+							if (key.channel() != null) {
+								key.channel().close();
+							}
+						}
+					}
+					
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -52,6 +74,47 @@ public class TimeServerHandler implements Runnable {
 		
 		
 
+	}
+
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			if (key.isAcceptable()) {
+				ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+				SocketChannel sc = ssc.accept();
+				sc.configureBlocking(false);
+				sc.register(selector, SelectionKey.OP_READ);
+			} if (key.isReadable()) {
+				SocketChannel sc = (SocketChannel) key.channel();
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out.println("time server received command: " + body);
+					String resp = "query time".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "bad command";
+					doWrite(sc, resp);
+				} else if (readBytes < 0) {
+					key.cancel();
+					sc.close();
+				} else {
+					
+				}
+			}
+		}
+		
+	}
+
+	private void doWrite(SocketChannel sc, String resp) throws IOException {
+		if (resp != null && resp.trim().length() > 0) {
+			byte[] bytes = resp.getBytes();
+			ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+			writeBuffer.put(bytes);
+			writeBuffer.flip();
+			sc.write(writeBuffer);
+		}
+		
 	}
 
 }
